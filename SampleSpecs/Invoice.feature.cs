77192fc241789e0274d412cae3f29d785d399db9
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2017-08-09 10:52:05 +0200

    Unopened invoice modification

--- SampleSpecs/Invoice.feature.cs
+++ SampleSpecs/Invoice.feature.cs
@@ -37,7 +37,7 @@ namespace SampleSpecs
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
             TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Support of invoicing for sales departament", @"   As a seller I want to be able to create an invoice to put ordered items on it
-   As a seller I want to be able to put item on invoice to calculate total sum to pay
+   As a seller I want to be able to put item on invoice to calculate total sum
    As a seller I want to be able to change invoice date to specify sale date
    As a seller I want to be able to close invoice so it cannot be changed", ProgrammingLanguage.CSharp, ((string[])(null)));
             testRunner.OnFeatureStart(featureInfo);
@@ -72,6 +72,24 @@ namespace SampleSpecs
         void System.IDisposable.Dispose()
         {
             this.ScenarioTearDown();
+        }
+        
+        [Xunit.FactAttribute(DisplayName="Unopened invoice modification")]
+        [Xunit.TraitAttribute("FeatureTitle", "Support of invoicing for sales departament")]
+        [Xunit.TraitAttribute("Description", "Unopened invoice modification")]
+        public virtual void UnopenedInvoiceModification()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Unopened invoice modification", ((string[])(null)));
+#line 7
+this.ScenarioSetup(scenarioInfo);
+#line 8
+   testRunner.Given("is an empty unopened invoice", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 9
+   testRunner.When("I try to add item to it", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 10
+   testRunner.Then("I should get an error \"You need to open invoice befor adding items.\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
         }
         
         [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "2.2.0.0")]
---
+++ SampleSpecs/SupportOfInvoicingForSalesDepartamentSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SampleSpecs
+{
+    [Binding]
+    public class SupportOfInvoicingForSalesDepartamentSteps
+    {
+        [Given(@"is an empty unopened invoice")]
+        public void GivenIsAnEmptyUnopenedInvoice()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I try to add item to it")]
+        public void WhenITryToAddItemToIt()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should get an error ""(.*)""")]
+        public void ThenIShouldGetAnError(string error)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
